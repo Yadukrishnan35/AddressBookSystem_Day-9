47235be411bafd6ee72eaa9dd98c5045bc77f11c
yadukrishnan <yadulive333@gmail.com>
2022-03-03 08:36:29 +0530

    Create contact

---
+++ Main.java
@@ -0,0 +1,48 @@
+package com.blz;
+
+import java.util.Scanner;
+
+public class Main {
+
+	 public Contacts createContacts() {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("Enter First Name:");
+			String firstName = sc.next();
+			
+			System.out.println("Enter Last Name:");
+			String lastName = sc.next();
+			
+			System.out.println("Enter address:");
+			String address = sc.next();
+			
+			System.out.println("Enter city:");
+			String city = sc.next();
+			
+			System.out.println("Enter state:");
+			String state = sc.next();
+			
+			System.out.println("Enter zip code:");
+			int zip = sc.nextInt();
+			
+			System.out.println("Enter phone No.:");
+			String phoneNumber = sc.next();
+			
+			System.out.println("Enter email address:");
+			String email = sc.next();
+			sc.close();
+			Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+			
+			return contact;		
+		}
+		
+		public static void main(String[] args) {
+			
+			Main addressBookObject = new Main();		
+			Contacts contact = addressBookObject.createContacts();
+			System.out.println("New Contact Created: " + "\n"+contact.toString());		
+		}
+
+	}
+
+
