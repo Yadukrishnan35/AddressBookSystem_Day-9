ad6feabd06ec3f3b212fdc18c5d8e798087f4866
yadukrishnan <yadulive333@gmail.com>
2022-03-03 09:00:31 +0530

    Update using Name

--- Main.java
+++ Main.java
@@ -13,9 +13,46 @@ public class Main {
 	}
 	
 	public void displayAddressBook() {
-		for(Map.Entry<String,Contacts> contact : addressBook.entrySet()) {
+		for(Map.Entry<String, Contacts> contact : addressBook.entrySet()) {
 			System.out.println(contact.getValue());
 		}
+	}
+	
+	public void editContact(String fullName) {
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		for(Map.Entry<String, Contacts> contact : addressBook.entrySet()) {
+			if(fullName.toUpperCase().equals((contact.getKey()).toUpperCase())) {
+				System.out.println("Which you want to edit: ");
+				System.out.println("1. Address");
+				System.out.println("2. City");
+				System.out.println("3. State");
+				System.out.println("4. Zip");
+				System.out.println("5. Phone Number");
+				System.out.println("6. Email");
+				int choice = sc.nextInt();
+				switch (choice) {
+				case 1: contact.getValue().setAddress(sc.next());
+					 	break;
+				case 2: contact.getValue().setCity(sc.next());
+						break;
+				case 3:	contact.getValue().setState(sc.next());
+						break;
+				case 4:	contact.getValue().setZip(sc.nextInt());
+						break;
+				case 5:	contact.getValue().setPhoneNumber(sc.next());
+						break;
+				case 6:	contact.getValue().setEmail(sc.next());
+						break;
+				default:System.out.println("Invaild choice");
+				}
+			flag=false;
+			break;
+			}
+		}
+		if(flag)
+			System.out.println("Contact doesn't Exist");
+		sc.close();		
 	}
 	
 	public Contacts createContact() {
@@ -53,15 +90,22 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Main addressBookObject = new Main();		
-		System.out.println("Add new Contact(Y/N): ");
-		String select = sc.next();
+
+		Contacts givenContact = new Contacts("Yadukrishnan" , "KP" , "aaaa" , "bbbb" , "Kerala" , 12345 , "98765432" , "abc123@gmail.com");
+		addressBookObject.addContactToAddressBook(givenContact);				
+
+		System.out.println("1. Add Contact \n2. Edit Contact\n ");
+		System.out.println("Enter your choice: ");
+		int select = sc.nextInt();
 		switch(select) {
-		case "Y": Contacts contact = addressBookObject.createContact();
-				  addressBookObject.addContactToAddressBook(contact);
-				  break;
-		case "N": System.out.println("Cancel to add Contact");
-				  break;
-				  default: System.out.println("Invalid choice");
+			case 1: Contacts contact = addressBookObject.createContact();
+					addressBookObject.addContactToAddressBook(contact);
+					break;
+			case 2: System.out.println("Enter Fullname: ");
+					String fullName = sc.nextLine(); 
+					addressBookObject.editContact(fullName);
+					break;
+			default: System.out.println("Invalid choice");
 		}
 		addressBookObject.displayAddressBook();
 		sc.close();
